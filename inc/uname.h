84a04abee5af2003e39fc1194d166634a268ad25
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2023-04-26 20:49:03 -0600

    add: uname header and empty source

---
+++ inc/uname.h
@@ -0,0 +1,6 @@
+/**
+ * Calls the uname command by using the system call
+*/
+
+
+int uname_command(char *arg_list[]);
---
+++ lib/uname.c
@@ -0,0 +1,5 @@
+#include <sys/utsname.h>  // uname command
+
+int uname_command(char *arg_list[]) {
+
+}
