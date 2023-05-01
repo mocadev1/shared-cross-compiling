1a27eeb611b581d7730d8c76b049338ec76f8916
Israel-SanchezH <ish.comunicaciones@gmail.com>
2023-04-30 19:09:56 -0600

    Update main file

--- new_Busybox.c
+++ new_Busybox.c
@@ -1,64 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "./inc/cat.h"
+// Prototypes for the module 1 exercises
+//int cp();
+//int uname();
+//int cat();
 
-int main(int argc, char *argv[]) {
-    pid_t pid;
-    int status;
+int main() {
+    int selection = -1;
 
-    // printf("Got %s...",argv[1]);
-    // Fork a child process
-    pid = fork();
+    while (selection != 0) {
+        printf("\n=== Module 1 ===\n");
+        printf("Select an exercise:\n");
+        printf("1. cp\n");
+        printf("2. uname\n");
+        printf("3. cat\n");
+        scanf("%d", &selection);
 
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Check if this is the child process
-        char *command_ex = NULL;
-
-        if (strcmp(argv[1], "sleep") == 0) {
-            command_ex = "./alex_sleep";
-        } else if (strcmp(argv[1], "uname") == 0) {
-            command_ex = "./alex_uname";
-        } else if (strcmp(argv[1], "ls") == 0) {
-            command_ex = "./alex_ls";
-        } else if (strcmp(argv[1], "cat") == 0) {
-            command_ex = "./alex_cat";
-        } else if (strcmp(argv[1], "lsmod") == 0) {
-            command_ex = "./alex_lsmod";
-        } else if (strcmp(argv[1], "uptime") == 0) {
-            command_ex = "./alex_uptime";
-        } else if (strcmp(argv[1], "chmod") == 0) {
-            command_ex = "./alex_chmod";
-        } else if (strcmp(argv[1], "cp") == 0) {
-            command_ex = "./alex_copy";
-        } else {
-            fprintf(stderr, "Unknown command: %s\n", argv[1]);
-            exit(EXIT_FAILURE);
-        }
-        char *args[] = {command_ex, argv[2], argv[3], argv[4], argv[5], NULL}; // Setup the arguments to be executed
-
-        execvp(args[0], args); // Execute the command with arguments
-
-        // If execvp returns, an error has occurred
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        // This is the parent process
-        // Wait for the child process to finish
-        waitpid(pid, &status, 0);
-
-        // Check if the child process exited normally
-        if (WIFEXITED(status)) {
-            printf("Child process exited with status %d\n", WEXITSTATUS(status));
-        } else {
-            printf("Child process terminated abnormally\n");
+        switch (selection) {
+            case 1:
+                //exercise1(argc[]);
+                break;
+            case 2:
+                //exercise2();
+                break;
+            case 3:
+                cat();
+                break;
+            case 0:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid selection. Please try again.\n");
+                break;
         }
     }
-
+    
     return 0;
 }
+
